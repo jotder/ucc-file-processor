46254e6908517a61108dd52ed178e557ff608a6e
jotder <jotder@gmail.com>
2026-05-27 22:19:21 +0530

    test: add JUnit 5 harness with surefire

---
+++ file-processor/src/test/java/com/gamma/SmokeTest.java
@@ -0,0 +1,11 @@
+package com.gamma;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+    @Test
+    void harnessRuns() {
+        assertEquals(2, 1 + 1);
+    }
+}
